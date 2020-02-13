92cfa2cdccf878f3cdbf60993815c798a62a963f
Zubair <zubair@sudofy.com>
2020-02-13 18:09:33 +0500

    adjusted reference issue

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ module.exports = function Random(params) {
       numberOfArrays
     } = options;
 
-    const isInvalidOptions = validation(options);
+    const isInvalidOptions = validation.validateOptions(options);
 
     if (isInvalidOptions) {
       return isInvalidOptions;
